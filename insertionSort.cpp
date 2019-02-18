b7cde21685c83cad4b9f0f2f10db8b582b0cd4e3
NahinHossain <43328112+NahinHossain@users.noreply.github.com>
2019-02-18 23:13:37 +0600

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -3,36 +3,36 @@ using namespace std;
 
 int main()
 {
-	int index,i,j,temp, num,arr[100];
-	
-	cout<<"How many elements do you want to enter? "<<endl;
-	cin>>num;
-	
+	int index,i,j,temp, number,arr[100];
+
+	cout<<"elements want ? "<<endl;
+	cin>>number;
+
 	cout<<"Enter the elements: "<<endl;
-	for(i=0;i<num;i++)
+	for(i=0;i<number;i++)
 	{
 		cin>>arr[i];
 	}
 
-	for(j=1;j<num;j++)
+	for(j=1;j<number;j++)
 	{
 		index=arr[j];
 		i=j-1;
 		while((i>=0)&&(arr[i]>=index))
 		{
 			temp=arr[i+1];
-			arr[i+1]=arr[i]; 
+			arr[i+1]=arr[i];
 			arr[i]=temp;
 			i=i-1;
 		}
 		arr[i+1]=index;
 	}
-	
-	cout<<"\nElements after sorting using insertion:  "<<endl;
-	for(i=0;i<num;i++)
+
+	cout<<"Elements after sorting using insertion:  "<<endl;
+	for(i=0;i<number;i++)
 	{
 		cout<<" "<<arr[i]<<endl;
 	}
-	
+
 	return 0;
 }
